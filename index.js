ccdaef54833ace27375f95c44903cbc506e67ec6
vishal <selvanvishal@gmail.com>
2025-02-20 21:22:32 +0530

    custom delimiter check

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ function add(numbers) {
         return 0;
     }
 
+    if (numbers.startsWith("//")) {
+        const delimiterLineEnd = numbers.indexOf("\n");
+        const delimiter = numbers.slice(2, delimiterLineEnd);
+        numbers = numbers.slice(delimiterLineEnd + 1);
+        return numbers.split(delimiter).map(Number).reduce((sum, num) => sum + num, 0);
+    }
+
     return numbers.split(/,|\n/).map(Number).reduce((sum, num) => sum + num, 0);
 }
