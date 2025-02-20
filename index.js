ffb1e681e30a8ebf9bbebe8a0dc5f177064a116b
vishal <selvanvishal@gmail.com>
2025-02-20 21:21:45 +0530

    delimiter addition

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function add(numbers) {
         return 0;
     }
 
-    return numbers.split(',').map(Number).reduce((sum, num) => sum + num, 0);
+    return numbers.split(/,|\n/).map(Number).reduce((sum, num) => sum + num, 0);
 }
